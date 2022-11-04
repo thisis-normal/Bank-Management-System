1ea680ef44f8019c7523df7fe3a48ab8fadcfb17
Normal <thuonghuunguyen2002@gmail.com>
2022-11-04 11:26:52 +0700

    Validated User's Transfer

--- User.cpp
+++ User.cpp
@@ -44,6 +44,16 @@ void saveFileUser()
         }
     }
 }
+void userMenu()
+{
+    cout << "+----------------------------------------------------------------------+" << endl;
+    cout << "|                      USER MANANGEMENT PROGRAM                        |" << endl;
+    cout << "+----------------------------------------------------------------------+" << endl;
+    cout << "|1. Display |2. Deposit |3. Withdraw  |4. Transaction |5. Exit & Save  |" << endl;
+    cout << "+----------------------------------------------------------------------+" << endl;
+    cout << "Enter your option: ";
+}
+
 class User : public BankSys
 {
 public:
@@ -65,32 +75,32 @@ public:
             }
         }
     }
-    void displayAll()
-    {
-        int count = 0;
-        cout << "Here is the list: " << endl;
-        printf("+---------------------------------------------------------------------+\n");
-        for (int i = 0; i < maxrow; i++)
-        {
-            if (bankName[i] != "\0")
-            {
-                cout << "Name: " << bankName[i] << "||"
-                     << "Balance: " << bankBalance[i] << "||"
-                     << "Phone: " << bankPhone[i] << "||"
-                     << "City: " << bankCity[i] << "||"
-                     << "AccountNumber: " << bankAccountNumber[i] << endl;
-                // printf("| %-12s | %-5s | %-10s | %-10s | %-10s |", bankName[i].c_str(), bankBalance[i].c_str(),  bankPhone[i].c_str(), bankCity[i].c_str(), bankAccountNumber[i].c_str());
-                cout << endl;
-                count++;
-            }
-        }
-        cout << "======================" << endl;
-        cout << "Total records: " << count << endl;
-        if (count == 0)
-        {
-            cout << "==> No records found" << endl;
-        }
-    }
+    // void displayAll()
+    // {
+    //     int count = 0;
+    //     cout << "Here is the list: " << endl;
+    //     printf("+---------------------------------------------------------------------+\n");
+    //     for (int i = 0; i < maxrow; i++)
+    //     {
+    //         if (bankName[i] != "\0")
+    //         {
+    //             cout << "Name: " << bankName[i] << "||"
+    //                  << "Balance: " << bankBalance[i] << "||"
+    //                  << "Phone: " << bankPhone[i] << "||"
+    //                  << "City: " << bankCity[i] << "||"
+    //                  << "AccountNumber: " << bankAccountNumber[i] << endl;
+    //             // printf("| %-12s | %-5s | %-10s | %-10s | %-10s |", bankName[i].c_str(), bankBalance[i].c_str(),  bankPhone[i].c_str(), bankCity[i].c_str(), bankAccountNumber[i].c_str());
+    //             cout << endl;
+    //             count++;
+    //         }
+    //     }
+    //     cout << "======================" << endl;
+    //     cout << "Total records: " << count << endl;
+    //     if (count == 0)
+    //     {
+    //         cout << "==> No records found" << endl;
+    //     }
+    // }
     //  deposit money to account with phone number
     void depositMoney(AccessControl *access)
     {
@@ -168,49 +178,64 @@ public:
         cout << "Enter destination phone: ";
         cin.ignore();
         getline(cin, destiPhone);
-        int money;
-        cout << "Enter money: ";
-        cin >> money;
-        for (int i = 0; i < maxrow; i++)
-        {
-            if (bankPhone[i] == phone)
-            {
-                int balance = stoi(bankBalance[i]);
-                if (balance <= 0)
-                {
-                    cout << "==> Your balance must be more than 0 to tranfer" << endl;
-                    return;
-                }
-                if (money > balance)
-                {
-                    cout << "==> Your balance is not enough to transfer" << endl;
-                    break;
-                }
-                balance -= money;
-                bankBalance[i] = to_string(balance);
-                cout << "Successfully decrease your balance" << endl;
-                break;
-            }
-        }
+        int count = 0;
         for (int i = 0; i < maxrow; i++)
         {
             if (bankPhone[i] == destiPhone)
             {
-                int balance = stoi(bankBalance[i]);
-                balance += money;
-                bankBalance[i] = to_string(balance);
-                cout << "Successfully increase destination balance" << endl;
-                break;
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            cout << "==> Destination phone number does not exist" << endl;
+            return;
+        }
+        else
+        {
+            if (phone == destiPhone)
+            {
+                cout << "==> You cannot transfer money to yourself" << endl;
+                return;
+            }
+            else
+            {
+                int money;
+                cout << "Enter money: ";
+                cin >> money;
+                for (int i = 0; i < maxrow; i++)
+                {
+                    if (bankPhone[i] == phone)
+                    {
+                        int balance = stoi(bankBalance[i]);
+                        if (balance <= 0)
+                        {
+                            cout << "==> Your balance must be more than 0 to tranfer" << endl;
+                            return;
+                        }
+                        if (money > balance)
+                        {
+                            cout << "==> Your balance is not enough to transfer" << endl;
+                            break;
+                        }
+                        balance -= money;
+                        bankBalance[i] = to_string(balance);
+                        cout << "Successfully decrease your balance" << endl;
+                        break;
+                    }
+                }
+                for (int i = 0; i < maxrow; i++)
+                {
+                    if (bankPhone[i] == destiPhone)
+                    {
+                        int balance = stoi(bankBalance[i]);
+                        balance += money;
+                        bankBalance[i] = to_string(balance);
+                        cout << "Successfully increase destination balance" << endl;
+                        break;
+                    }
+                }
             }
         }
     }
 };
-void userMenu()
-{
-    cout << "+----------------------------------------------------------------------+" << endl;
-    cout << "|                      USER MANANGEMENT PROGRAM                        |" << endl;
-    cout << "+----------------------------------------------------------------------+" << endl;
-    cout << "|1. Display |2. Deposit |3. Withdraw  |4. Transaction |5. Exit & Save  |" << endl;
-    cout << "+----------------------------------------------------------------------+" << endl;
-    cout << "Enter your option: ";
-}
